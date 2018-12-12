d674bc7a2ee751fb8ad4673845238e8c368fe6a4
Thomas Trimble <ttrimble@fb.com>
2018-12-11 23:00:26 -0800

    Refactored slightly

--- resume-builder/src/App.js
+++ resume-builder/src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
+
 import './App.css';
-
-import resume_json from './data/trimblet.json';
-
-const { Map } = require('immutable');
 
 const { Header } = require('./components/header.react');
 const { Experience } = require('./components/experience.react');
@@ -12,12 +8,11 @@ const { Experience } = require('./components/experience.react');
 class App extends Component {
 
   render() {
-    const resume = Map(resume_json);
     return (
       <div className="App">
-        <Header info={resume.get("info")} />
-        <Experience entries={resume.get("experience")} />
-        <Experience entries={resume.get("education")} />
+        <Header info={this.props.resume.get("info")} />
+        <Experience entries={this.props.resume.get("experience")} />
+        <Experience entries={this.props.resume.get("education")} />
       </div>
     );
   }
--- resume-builder/src/index.js
+++ resume-builder/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import resume_json from './data/trimblet.json';
+
+const { Map } = require('immutable');
+
+ReactDOM.render(<App resume={Map(resume_json)} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
